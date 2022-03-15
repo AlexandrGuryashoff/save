4a4de4f2e96a7779c38ec9ac13b1e66cd21a1b22
Александр Гурьяшов <a.guryashoff@yandex.ru>
2022-03-15 21:31:41 +0300

    путь в отдельной константе

--- Main.java
+++ Main.java
@@ -11,20 +11,24 @@ import java.util.zip.ZipOutputStream;
 public class Main {
 
     public static void main(String[] args) {
+        String path1 = "E://Games//savegames//game1.dat";
+        String path2 = "E://Games//savegames//game2.dat";
+        String path3 = "E://Games//savegames//game3.dat";
+
 	  GameProgress game1 = new GameProgress(12, 100, 4, 102.8);
       GameProgress game2 = new GameProgress(25, 400, 7, 302.64);
       GameProgress game3 = new GameProgress(90, 700, 10, 848.59);
-      saveGame("E://Games//savegames//game1.dat", game1);
-      saveGame("E://Games//savegames//game2.dat", game2);
-      saveGame("E://Games//savegames//game3.dat", game3);
+      saveGame(path1, game1);
+      saveGame(path2, game2);
+      saveGame(path3, game3);
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("E://Games//savegames//game1.dat");
-        arrayList.add("E://Games//savegames//game2.dat");
-        arrayList.add("E://Games//savegames//game3.dat");
+        arrayList.add(path1);
+        arrayList.add(path2);
+        arrayList.add(path3);
         zipFiles("E://Games//savegames//zip.zip", arrayList);
-        File game1Dat = new File("E://Games//savegames//game1.dat");
-        File game2Dat = new File("E://Games//savegames//game2.dat");
-        File game3Dat = new File("E://Games//savegames//game3.dat");
+        File game1Dat = new File(path1);
+        File game2Dat = new File(path2);
+        File game3Dat = new File(path3);
         if (game1Dat.delete()) System.out.println("Файл\"game1.dat\"удален");
         if (game2Dat.delete()) System.out.println("Файл\"game2.dat\"удален");
         if (game3Dat.delete()) System.out.println("Файл\"game3.dat\"удален");
